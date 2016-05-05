f377b0a37f5444ca3ede176628b63241919212c4
Trevor Gale <gale.t@husky.neu.edu>
2016-05-05 10:51:37 -0400

    Created interface for generator, callbacks for trace printing, separate lib file for only preprocessing functions and example config for generator

---
+++ lib/PreProcessing.py
@@ -0,0 +1,57 @@
+""" filename: PreProcessing.py
+    contents: this files contains the routines used by "TraceGenerator" to
+    mix application profiles
+    
+    author: Trevor Gale
+    date: 3.5.16"""
+
+import numpy as np
+
+def BuildAlphaForest(appProfiles, alphaForest, weights, alphaRatio):
+    """ BuildAlphaForest: combines all alpha values for the input application
+        profiles and builds the alphaForest accordingly
+        
+        args:
+            - appProfiles: list of open file handles for each application profile
+            - alphaForest: initialized list of AlphaTrees
+            - weights: python list specifying the weights of each profile
+            - alphaRatio: ratio of num_reuse_distances / alphaTree
+            
+        returns: alphaRatio to use for generation"""
+    # get number of profiles
+    numProfiles = len(appProfiles)
+    numAlpha = len(alphaForest[0].reuseCount)
+    
+    # structures for alphaRatios and all alpha values
+    allAlphaRatios = np.zeros(len(appProfiles), dtype = np.float)
+    alphaValues = []
+
+    weightSum = 0
+    for i in xrange(numProfiles):
+        # store alphaRatio and alpha values
+        allAlphaRatios[i] = appProfiles[i]['alphaRatio'][()]
+        alphaValues.append(appProfiles[i]['alphas'])
+        
+        # sum up all weights
+        weightSum += weights[i]
+    
+    # for each tree in the alphaForest
+    for i in xrange(len(alphaForest)):
+        alphas = np.zeros(numAlpha, dtype = np.float)
+        reuseDistance = i * alphaRatio
+
+        # generate alpha values 
+        for j in xrange(numProfiles):
+            if allAlphaRatios[j] == float('Inf'):
+                index = 0
+            else:
+                index = reuseDistance/allAlphaRatios[j]
+                
+            alphas = np.add(alphas, alphaValues[j][index] \
+               * weights[j])
+        
+        # normalize by weightSum
+        alphas /= weightSum
+        
+        # load into this tree
+        alphaForest[i].LoadAlphas(alphas)
---
+++ lib/TraceFormats.py
@@ -0,0 +1,21 @@
+""" filename: TraceFormats
+    contents: this file contains functions passed as callbacks to
+    \"GenerateSyntheticTrace\" that are used to print the trace.
+    all functiosn take in the arguments (traceFile, cycle, accessType, 
+    memAddress), where traceFile is the handle to print the accesss to,
+    cycle is an integer, accessType is a 0 or 1 for load and store
+    respectively, and memAddress is a 4-byte word address
+    
+    author: Trevor Gale
+    date: 3.5.16"""
+    
+def STL(traceFile, cycle, accessType, memAddress):
+    """ STL: prints the memory reference in the format used in
+        socket transaction language (STL)"""
+    # handle read
+    if not accessType:
+        traceFile.write("%d: read 0x%x\n" % (cycle, memAddress))
+    else:
+        # default data for write is 0xABCD
+        traceFile.write("%d: write 0x%x 0xABCD\n" % (cycle, memAddress))
+        
